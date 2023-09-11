dfdb1c14862d3ee9959b010ac754ca7113c1b85a
Rijadi02 <adiveni0@gmail.com>
2023-09-11 16:30:39 +0200

    Added Crud for Company Users

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +10,9 @@ class Company extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CompanyUserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,7 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('companies', CompanyController::class)->middleware('isAdmin');
-
+    Route::resource('companies.users', CompanyUserController::class)->except('show');
 });
 
 
