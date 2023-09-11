13d1cbbae756100e888f69d8ca7c38ccc14459a0
Rijadi02 <adiveni0@gmail.com>
2023-09-11 12:55:09 +0200

    Added Middleware isAdmin

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('companies', CompanyController::class)->middleware('isAdmin');
+
 });
 
-Route::resource('companies', CompanyController::class);
+
 
 require __DIR__.'/auth.php';
